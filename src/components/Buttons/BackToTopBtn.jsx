0683a02d451e09b6140798c596ab98a8d40569b6
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-26 23:08:03 +0000

    split template to jsx components

---
+++ src/components/Buttons/BackToTopBtn.jsx
@@ -0,0 +1,12 @@
+export default function BackToTopBtn() {
+  return (
+    <>
+      <a
+        href="#"
+        className="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"
+      >
+        <i className="bi bi-arrow-up"></i>
+      </a>
+    </>
+  );
+}
