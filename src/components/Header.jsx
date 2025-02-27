d117914f895774f9ee6230e5fde2e82721cde482
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-27 23:29:29 +0000

    update header layout - add search by location and vehicle type

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,20 +17,61 @@ export default function Header() {
             className={"col-lg-7 p-5 wow fadeIn " + styles.section}
             data-wow-delay="0.4s"
           >
-            <h2 className="display-5 text-dark mb-5">
+            <h2 className="display-5 text-dark mb-4">
               <a className="display-6 text-dark" href="#">
                 Reserve a Vehicle
               </a>{" "}
               {}
               <span className={styles.orSpan}>or</span> {}
               <a href="#">
-                <span className={styles.textGreen}>View Reservation</span>
+                <span className={styles.textGreen}>
+                  View/Modify/Cancel Reservation
+                </span>
               </a>
             </h2>
-            <p className={"display-6 text-dark mb-5 " + styles.orSpan}>
+
+            {/* New */}
+
+            <form className="display-5 text-dark mb-5">
+              <div className="row g-4 ">
+                <div className="col-12 col-sm-5">
+                  <select
+                    className="form-select border-0"
+                    style={{ height: "55px" }}
+                  >
+                    <option defaultChecked>Choose location</option>
+                    <option value="1">Location 1</option>
+                    <option value="2">Location 2</option>
+                    <option value="3">Location 3</option>
+                  </select>
+                </div>
+                <div className="col-12 col-sm-5">
+                  <select
+                    className="form-select border-0"
+                    style={{ height: "55px" }}
+                  >
+                    <option defaultChecked>Choose Vehicle Type</option>
+                    <option value="1">Vehicle 1</option>
+                    <option value="2">Vehicle 2</option>
+                    <option value="3">Vehicle 3</option>
+                  </select>
+                </div>
+                <div className="col-12 col-sm-2">
+                  <button
+                    className={"btn btn-primary w-100 py-3 " + styles.searchBtn}
+                    type="submit"
+                  >
+                    Search
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            {/* New */}
+            {/* <p className={"display-2 text-dark mb-4 " + styles.subHeading}>
               Unlock the road to your next adventure
-            </p>
-            <div className="row g-4">
+            </p> */}
+            {/* <div className="row g-4">
               <div className="col-sm-4">
                 <div className="border-start border-dark ps-4">
                   <h2 className="text-dark mb-1" data-toggle="counter-up">
@@ -55,7 +96,7 @@ export default function Header() {
                   <p className="text-dark mb-0">CarClub Members</p>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
             <div className="owl-carousel header-carousel">
