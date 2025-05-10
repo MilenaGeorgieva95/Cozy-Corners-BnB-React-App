49988e6c9d82aba7d5f2cdee9badf67edc43e1c3
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-05-10 14:51:08 +0300

    add title to cars catalog page

--- client/src/components/catalog/catalog-cars/CatalogCars.jsx
+++ client/src/components/catalog/catalog-cars/CatalogCars.jsx
@@ -7,7 +7,16 @@ export default function CatalogCars() {
 
   return (
     <>
-      <h2>Cars Catalog</h2>
+      <div
+        className="text-center mx-auto wow fadeInUp"
+        data-wow-delay="0.1s"
+        style={{ maxWidth: "70%", padding: "10px" }}
+      >
+        <h3 className="display-6 mb-2 ">Our Fleet of Rental Cars</h3>
+        <span className="text-muted medium mb-2">
+          Find the Perfect Car for Your Journey
+        </span>
+      </div>
       {cars.map((car) => (
         <Card key={car._id} car={car} />
       ))}
